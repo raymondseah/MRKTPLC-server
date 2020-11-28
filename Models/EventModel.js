82a5b571c04c4f54aecd950db66a599aa5d1a48d
raymondseah <seah_raymond@hotmail.com>
2020-11-28 14:20:11 +0800

    started event models

--- Models/EventModel.js
+++ Models/EventModel.js
@@ -1,1 +1,25 @@
 'use strict'
+
+
+const mongoose = require('mongoose')
+
+const EventsSchema = new mongoose.Schema({
+    location: {
+        type: String,
+        required: true,
+    },
+    created_at: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+const EventsModel = mongoose.model('Events', EventsSchema)
+
+module.exports = EventsModel
